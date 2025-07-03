8d557af38e9533e9dee8b0d185646fd937d48720
arsoioioi <s1320150@u-aizu.ac.jp>
2025-07-03 11:50:26 +0900

    Dice game: initial revision

--- DiceGame.java
+++ DiceGame.java
@@ -1,13 +1,7 @@
 import java.util.Random;
-import java.util.Scanner;
 
 public class DiceGame {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("What is your name?");
-        String name = scanner.nextLine();
-        System.out.println("Hello, " + name + "!");
-
         System.out.println("Rolling dice...");
         Random rand = new Random();
         int die1 = rand.nextInt(6) + 1;
@@ -15,13 +9,5 @@ public class DiceGame {
         System.out.println("Die 1: " + die1);
         System.out.println("Die 2: " + die2);
         System.out.println("Total value: " + (die1 + die2));
-        int total = die1 + die2;
-        System.out.println("Total value: " + total);
-
-        if (total > 7) {
-            System.out.println("You won!");
-        } else {
-            System.out.println("You lost!");
-        }
     }
 }
